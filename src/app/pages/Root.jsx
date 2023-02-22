650bc957b1e9b15e2b7b0f4958b42a3c8a8995ad
seojinwan <jinwanseo@gmail.com>
2023-02-23 00:22:51 +0900

    add styled-component, react hook form

--- src/app/pages/Root.jsx
+++ src/app/pages/Root.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: teal;
+`;
 
 function Root(props) {
-  return <div>Install Styled Component</div>;
+  return <Title>Install Styled Component!</Title>;
 }
 
 export default Root;
