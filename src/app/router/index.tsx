c49ed87181822ff72405df9581e402cca04a9056
seojinwan <jinwanseo@gmail.com>
2023-04-01 09:11:40 +0900

    router 업데이트

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,18 +1,24 @@
+import { basename } from "path";
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../../pages/Root";
 import SpeechComponent from "../../pages/Speech";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root name="jinwan" age={3} />,
+      children: [
+        {
+          path: "speech",
+          element: <SpeechComponent />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Root name="jinwan" age={3} />,
-    children: [
-      {
-        path: "speech",
-        element: <SpeechComponent />,
-      },
-    ],
-  },
-]);
+    basename: process.env.PUBLIC_URL,
+  }
+);
 
 export default router;
